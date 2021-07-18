5ed02e60a70fdf4835a59db025c4563f539e24ad
Ckrenzer <crkrenz72@gmail.com>
2021-07-18 15:13:45 -0400

    porter_stemmer() now allows you to specify which column to stem

--- scripts/functions/porter_stemmer().R
+++ scripts/functions/porter_stemmer().R
@@ -1,5 +1,5 @@
 # Remember as you read this that replacement operations inside the mutate calls refer to entire vectors of words while the operations inside a case_when() call refers to individual words
-porter_stemmer <- function(unigram_df, text_column){
+porter_stemmer <- function(unigram_df, text_column = "word"){
   # The implementation of the Porter stemmer algorithm as I understand it,
   # as described in:
   # https://vijinimallawaarachchi.com/2017/05/09/porter-stemming-algorithm/
@@ -9,127 +9,127 @@ porter_stemmer <- function(unigram_df, text_column){
   
   # Step 1a
   unigram_df <- unigram_df %>% 
-    mutate(m = calculate_m(word),
+    mutate(m = calculate_m(get(text_column)),
            {{text_column}} := {case_when(
-             str_detect(word, "sses$") ~ str_replace(word, "sses$", "ss"),
-             str_detect(word, "ies$") ~ str_replace_all(word, "ies$", "i"),
-             str_detect(word, "([^s])s{1,1}$") ~ str_remove(word, "s$"),
+             str_detect(get(text_column), "sses$") ~ str_replace(get(text_column), "sses$", "ss"),
+             str_detect(get(text_column), "ies$") ~ str_replace_all(get(text_column), "ies$", "i"),
+             str_detect(get(text_column), "([^s])s{1,1}$") ~ str_remove(get(text_column), "s$"),
              
-             TRUE ~ word
+             TRUE ~ get(text_column)
            )})
   
   
   # Step 1b
   unigram_df <- unigram_df %>% 
-    mutate(m = calculate_m(word),
+    mutate(m = calculate_m(get(text_column)),
            {{text_column}} := {case_when(
-             m > 0 & str_detect(word, "eed$") ~ str_replace(word, "eed$", "ee"),
-             str_detect(word, "ed$") & str_detect(str_sub(word, end = -3), "[aeiou]") ~ step_1b_helper(word = word, suffix = "ed$", m = m),
-             str_detect(word, "ing$") & str_detect(str_sub(word, end = -4), "[aeiou]") ~ step_1b_helper(word = word, suffix = "ing$", m = m),
+             m > 0 & str_detect(get(text_column), "eed$") ~ str_replace(get(text_column), "eed$", "ee"),
+             str_detect(get(text_column), "ed$") & str_detect(str_sub(get(text_column), end = -3), "[aeiou]") ~ step_1b_helper(word = get(text_column), suffix = "ed$", m = m),
+             str_detect(get(text_column), "ing$") & str_detect(str_sub(get(text_column), end = -4), "[aeiou]") ~ step_1b_helper(word = get(text_column), suffix = "ing$", m = m),
              
-             TRUE ~ word
+             TRUE ~ get(text_column)
            )})
   
   
   # Step 1c
   unigram_df <- unigram_df %>% 
-    mutate({{text_column}} := {case_when(str_detect(word, "y$") & str_detect(str_sub(word, end = -1), "[aeiou]") ~ str_replace(string = word, pattern = "y$", replacement = "i"),
-                             TRUE ~ word)})
+    mutate({{text_column}} := {case_when(str_detect(get(text_column), "y$") & str_detect(str_sub(get(text_column), end = -1), "[aeiou]") ~ str_replace(string = get(text_column), pattern = "y$", replacement = "i"),
+                                         TRUE ~ get(text_column))})
   
   # Step 2
   unigram_df <- unigram_df %>% 
-    mutate(m = calculate_m(word),
+    mutate(m = calculate_m(get(text_column)),
            {{text_column}} := {case_when(
-      m > 0 & str_detect(word, "ational$") ~ str_replace(string = word, pattern = "ational$", replacement = "ate"),
-      m > 0 & str_detect(word, "tional$") ~ str_replace(string = word, pattern = "tional$", replacement = "tion"),
-      m > 0 & str_detect(word, "enci$") ~ str_replace(string = word, pattern = "enci$", replacement = "ence"),
-      m > 0 & str_detect(word, "anci$") ~ str_replace(string = word, pattern = "anci$", replacement = "ance"),
-      m > 0 & str_detect(word, "izer$") ~ str_replace(string = word, pattern = "izer$", replacement = "ize"),
-      m > 0 & str_detect(word, "abli$") ~ str_replace(string = word, pattern = "abli$", replacement = "able"),
-      m > 0 & str_detect(word, "alli$") ~ str_replace(string = word, pattern = "alli$", replacement = "al"),
-      m > 0 & str_detect(word, "entli$") ~ str_replace(string = word, pattern = "entli$", replacement = "ent"),
-      m > 0 & str_detect(word, "eli$") ~ str_replace(string = word, pattern = "eli$", replacement = "e"),
-      m > 0 & str_detect(word, "ousli$") ~ str_replace(string = word, pattern = "ousli$", replacement = "ous"),
-      m > 0 & str_detect(word, "ization$") ~ str_replace(string = word, pattern = "ization$", replacement = "ize"),
-      m > 0 & str_detect(word, "ation$") ~ str_replace(string = word, pattern = "ation$", replacement = "ate"),
-      m > 0 & str_detect(word, "ator$") ~ str_replace(string = word, pattern = "ator$", replacement = "ate"),
-      m > 0 & str_detect(word, "alism$") ~ str_replace(string = word, pattern = "alism$", replacement = "al"),
-      m > 0 & str_detect(word, "iveness$") ~ str_replace(string = word, pattern = "iveness$", replacement = "ive"),
-      m > 0 & str_detect(word, "fulness$") ~ str_replace(string = word, pattern = "fulness$", replacement = "ful"),
-      m > 0 & str_detect(word, "ousness$") ~ str_replace(string = word, pattern = "ousness$", replacement = "ous"),
-      m > 0 & str_detect(word, "aliti$") ~ str_replace(string = word, pattern = "aliti$", replacement = "al"),
-      m > 0 & str_detect(word, "iviti$") ~ str_replace(string = word, pattern = "iviti$", replacement = "ive"),
-      m > 0 & str_detect(word, "biliti$") ~ str_replace(string = word, pattern = "biliti$", replacement = "ble"),
-      
-      TRUE ~ word
-    )})
+             m > 0 & str_detect(get(text_column), "ational$") ~ str_replace(string = get(text_column), pattern = "ational$", replacement = "ate"),
+             m > 0 & str_detect(get(text_column), "tional$") ~ str_replace(string = get(text_column), pattern = "tional$", replacement = "tion"),
+             m > 0 & str_detect(get(text_column), "enci$") ~ str_replace(string = get(text_column), pattern = "enci$", replacement = "ence"),
+             m > 0 & str_detect(get(text_column), "anci$") ~ str_replace(string = get(text_column), pattern = "anci$", replacement = "ance"),
+             m > 0 & str_detect(get(text_column), "izer$") ~ str_replace(string = get(text_column), pattern = "izer$", replacement = "ize"),
+             m > 0 & str_detect(get(text_column), "abli$") ~ str_replace(string = get(text_column), pattern = "abli$", replacement = "able"),
+             m > 0 & str_detect(get(text_column), "alli$") ~ str_replace(string = get(text_column), pattern = "alli$", replacement = "al"),
+             m > 0 & str_detect(get(text_column), "entli$") ~ str_replace(string = get(text_column), pattern = "entli$", replacement = "ent"),
+             m > 0 & str_detect(get(text_column), "eli$") ~ str_replace(string = get(text_column), pattern = "eli$", replacement = "e"),
+             m > 0 & str_detect(get(text_column), "ousli$") ~ str_replace(string = get(text_column), pattern = "ousli$", replacement = "ous"),
+             m > 0 & str_detect(get(text_column), "ization$") ~ str_replace(string = get(text_column), pattern = "ization$", replacement = "ize"),
+             m > 0 & str_detect(get(text_column), "ation$") ~ str_replace(string = get(text_column), pattern = "ation$", replacement = "ate"),
+             m > 0 & str_detect(get(text_column), "ator$") ~ str_replace(string = get(text_column), pattern = "ator$", replacement = "ate"),
+             m > 0 & str_detect(get(text_column), "alism$") ~ str_replace(string = get(text_column), pattern = "alism$", replacement = "al"),
+             m > 0 & str_detect(get(text_column), "iveness$") ~ str_replace(string = get(text_column), pattern = "iveness$", replacement = "ive"),
+             m > 0 & str_detect(get(text_column), "fulness$") ~ str_replace(string = get(text_column), pattern = "fulness$", replacement = "ful"),
+             m > 0 & str_detect(get(text_column), "ousness$") ~ str_replace(string = get(text_column), pattern = "ousness$", replacement = "ous"),
+             m > 0 & str_detect(get(text_column), "aliti$") ~ str_replace(string = get(text_column), pattern = "aliti$", replacement = "al"),
+             m > 0 & str_detect(get(text_column), "iviti$") ~ str_replace(string = get(text_column), pattern = "iviti$", replacement = "ive"),
+             m > 0 & str_detect(get(text_column), "biliti$") ~ str_replace(string = get(text_column), pattern = "biliti$", replacement = "ble"),
+             
+             TRUE ~ get(text_column)
+           )})
   
   
   
   # Step 3
   unigram_df <- unigram_df %>% 
-    mutate(m = calculate_m(word),
+    mutate(m = calculate_m(get(text_column)),
            {{text_column}} := {case_when(
-      m > 0 & str_detect(word, "icate$") ~ str_replace(string = word, pattern = "icate$", replacement = "ic"),
-      m > 0 & str_detect(word, "ative$") ~ str_remove(string = word, pattern = "ative$"),
-      m > 0 & str_detect(word, "alize$") ~ str_replace(string = word, pattern = "alize$", replacement = "al"),
-      m > 0 & str_detect(word, "iciti$") ~ str_replace(string = word, pattern = "iciti$", replacement = "ic"),
-      m > 0 & str_detect(word, "ical$") ~ str_replace(string = word, pattern = "ical$", replacement = "ic"),
-      m > 0 & str_detect(word, "ful$") ~ str_remove(string = word, pattern = "ful$"),
-      m > 0 & str_detect(word, "ness$") ~ str_remove(string = word, pattern = "ness$"),
-      
-      TRUE ~ word
-    )})
+             m > 0 & str_detect(get(text_column), "icate$") ~ str_replace(string = get(text_column), pattern = "icate$", replacement = "ic"),
+             m > 0 & str_detect(get(text_column), "ative$") ~ str_remove(string = get(text_column), pattern = "ative$"),
+             m > 0 & str_detect(get(text_column), "alize$") ~ str_replace(string = get(text_column), pattern = "alize$", replacement = "al"),
+             m > 0 & str_detect(get(text_column), "iciti$") ~ str_replace(string = get(text_column), pattern = "iciti$", replacement = "ic"),
+             m > 0 & str_detect(get(text_column), "ical$") ~ str_replace(string = get(text_column), pattern = "ical$", replacement = "ic"),
+             m > 0 & str_detect(get(text_column), "ful$") ~ str_remove(string = get(text_column), pattern = "ful$"),
+             m > 0 & str_detect(get(text_column), "ness$") ~ str_remove(string = get(text_column), pattern = "ness$"),
+             
+             TRUE ~ get(text_column)
+           )})
   
   
   
   # Step 4
   unigram_df <- unigram_df %>% 
-    mutate(m = calculate_m(word),
+    mutate(m = calculate_m(get(text_column)),
            {{text_column}} := {case_when(
-      m > 1 & str_detect(word, "al$") ~ str_remove(string = word, pattern = "al$"),
-      m > 1 & str_detect(word, "ance$") ~ str_remove(string = word, pattern = "ance$"),
-      m > 1 & str_detect(word, "ence$") ~ str_remove(string = word, pattern = "ence$"),
-      m > 1 & str_detect(word, "er$") ~ str_remove(string = word, pattern = "er$"),
-      m > 1 & str_detect(word, "ic$") ~ str_remove(string = word, pattern = "ic$"),
-      m > 1 & str_detect(word, "able$") ~ str_remove(string = word, pattern = "able$"),           
-      m > 1 & str_detect(word, "ible$") ~ str_remove(string = word, pattern = "ible$"),           
-      m > 1 & str_detect(word, "ant$") ~ str_remove(string = word, pattern = "ant$"),           
-      m > 1 & str_detect(word, "ement$") ~ str_remove(string = word, pattern = "ement$"),           
-      m > 1 & str_detect(word, "ment$") ~ str_remove(string = word, pattern = "ment$"), 
-      m > 1 & str_detect(word, "ent$") ~ str_remove(string = word, pattern = "ent$"), 
-      m > 1 & str_detect(word, "tion$|sion$") ~ str_remove(string = word, pattern = "ion$"),           
-      m > 1 & str_detect(word, "ou$") ~ str_remove(string = word, pattern = "ou$"),           
-      m > 1 & str_detect(word, "ism$") ~ str_remove(string = word, pattern = "ism$"),           
-      m > 1 & str_detect(word, "ate$") ~ str_remove(string = word, pattern = "ate$"),           
-      m > 1 & str_detect(word, "iti$") ~ str_remove(string = word, pattern = "iti$"),           
-      m > 1 & str_detect(word, "ous$") ~ str_remove(string = word, pattern = "ous$"),           
-      m > 1 & str_detect(word, "ive$") ~ str_remove(string = word, pattern = "ive$"),           
-      m > 1 & str_detect(word, "ize$") ~ str_remove(string = word, pattern = "ize$"), 
-      
-      TRUE ~ word
-    )})
+             m > 1 & str_detect(get(text_column), "al$") ~ str_remove(string = get(text_column), pattern = "al$"),
+             m > 1 & str_detect(get(text_column), "ance$") ~ str_remove(string = get(text_column), pattern = "ance$"),
+             m > 1 & str_detect(get(text_column), "ence$") ~ str_remove(string = get(text_column), pattern = "ence$"),
+             m > 1 & str_detect(get(text_column), "er$") ~ str_remove(string = get(text_column), pattern = "er$"),
+             m > 1 & str_detect(get(text_column), "ic$") ~ str_remove(string = get(text_column), pattern = "ic$"),
+             m > 1 & str_detect(get(text_column), "able$") ~ str_remove(string = get(text_column), pattern = "able$"),           
+             m > 1 & str_detect(get(text_column), "ible$") ~ str_remove(string = get(text_column), pattern = "ible$"),           
+             m > 1 & str_detect(get(text_column), "ant$") ~ str_remove(string = get(text_column), pattern = "ant$"),           
+             m > 1 & str_detect(get(text_column), "ement$") ~ str_remove(string = get(text_column), pattern = "ement$"),           
+             m > 1 & str_detect(get(text_column), "ment$") ~ str_remove(string = get(text_column), pattern = "ment$"), 
+             m > 1 & str_detect(get(text_column), "ent$") ~ str_remove(string = get(text_column), pattern = "ent$"), 
+             m > 1 & str_detect(get(text_column), "tion$|sion$") ~ str_remove(string = get(text_column), pattern = "ion$"),           
+             m > 1 & str_detect(get(text_column), "ou$") ~ str_remove(string = get(text_column), pattern = "ou$"),           
+             m > 1 & str_detect(get(text_column), "ism$") ~ str_remove(string = get(text_column), pattern = "ism$"),           
+             m > 1 & str_detect(get(text_column), "ate$") ~ str_remove(string = get(text_column), pattern = "ate$"),           
+             m > 1 & str_detect(get(text_column), "iti$") ~ str_remove(string = get(text_column), pattern = "iti$"),           
+             m > 1 & str_detect(get(text_column), "ous$") ~ str_remove(string = get(text_column), pattern = "ous$"),           
+             m > 1 & str_detect(get(text_column), "ive$") ~ str_remove(string = get(text_column), pattern = "ive$"),           
+             m > 1 & str_detect(get(text_column), "ize$") ~ str_remove(string = get(text_column), pattern = "ize$"), 
+             
+             TRUE ~ get(text_column)
+           )})
   
   
   
   # Step 5a
   unigram_df <- unigram_df %>% 
-    mutate(m = calculate_m(word),
+    mutate(m = calculate_m(get(text_column)),
            {{text_column}} := {case_when(
-      m > 1 & str_detect(word, "e$") ~ str_remove(word, "e$"),
-      m == 1 & !str_detect(word, "[^aeiou]{1}[aeiou]{1}[^aeiou]{1}e$") & str_detect(word, "e$") ~ str_remove(word, "e$"),
-      
-      TRUE ~ word
-      
-    )})
+             m > 1 & str_detect(get(text_column), "e$") ~ str_remove(get(text_column), "e$"),
+             m == 1 & !str_detect(get(text_column), "[^aeiou]{1}[aeiou]{1}[^aeiou]{1}e$") & str_detect(get(text_column), "e$") ~ str_remove(get(text_column), "e$"),
+             
+             TRUE ~ get(text_column)
+             
+           )})
   
   
   # Step 5b
   unigram_df <- unigram_df %>% 
-    mutate(m = calculate_m(word),
-           {{text_column}} := {case_when(m > 1 & str_detect(word, "ll$") ~ str_replace(string = word, pattern = "ll$", replacement = "l"),
-                             TRUE ~ word
-    )})
+    mutate(m = calculate_m(get(text_column)),
+           {{text_column}} := {case_when(m > 1 & str_detect(get(text_column), "ll$") ~ str_replace(string = get(text_column), pattern = "ll$", replacement = "l"),
+                                         TRUE ~ get(text_column)
+           )})
   
   
   return(unigram_df)
