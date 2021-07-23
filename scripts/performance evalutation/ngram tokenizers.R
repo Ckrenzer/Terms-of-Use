6e5982390c33733f8079cac63890f3e8d90b9888
Ckrenzer <crkrenz72@gmail.com>
2021-07-22 21:26:27 -0400

    finished custom ngram tokenizers

---
+++ scripts/performance evalutation/ngram tokenizers.R
@@ -0,0 +1,118 @@
+# Your unnest_tokens() implementation
+pacman::p_load(stringr, data.table, tidytext, dplyr, microbenchmark)
+# Four implementations--any functions you don't recognize belong to data.table
+
+
+# The text
+"words in a brief sentence"
+# The text as a one-column data frame
+df <- tibble(value = "words in a brief sentence")
+
+
+
+# Fastest unigram tokenizer ----------------------------------------------
+# To get unigrams, we need to split the words up into a vector.
+# Indexing is faster than coercion:
+microbenchmark(
+  str_split("words in a brief sentence", "\\s+")[[1]],
+  
+  unlist(str_split("words in a brief sentence", "\\s+")),
+  
+  str_split("words in a brief sentence", "\\s+") %>% unlist,
+  
+  times = 10000,
+  check = "identical"
+)
+
+
+
+
+
+# N-gram Algorithms ------------------------------------------------------
+# Works for bigrams only
+f1 <- function(words = df$value){
+  # the unigrams
+  words <- str_split(words, "\\s+")[[1]]
+  
+  tibble(text = str_c(head(words, -1), tail(words, -1), sep = " ", collapse = NULL))
+}
+f1()
+
+
+# My loop implementation (no one helped me put this together)
+f2 <- function(words = df$value, n = 2){
+  # making unigrams
+  words <- str_split(words, "\\s+")[[1]]
+  n <- n - 1
+  
+  vec <- character(length(words) - n)
+  for(i in 1:length(vec)){
+    for(j in i:(i + n)){
+      vec[i] <- str_c(vec[i], words[j], sep = " ") 
+    }
+  }
+  vec <- str_remove(vec, "^\\s{1}")
+  return(tibble(text = vec))
+}
+f2()
+
+
+# Does not work for unigrams
+f3 <- function(words = df$value, n = 2){
+  # the unigrams
+  words <- str_split(words, "\\s+")[[1]]
+  n <- n - 1
+  
+  lst1 <- lapply(shift(words, n = 0:n, type = 'lead'), na.omit)
+  mn <- min(lengths(lst1))
+  grams <- do.call(paste,  lapply(lst1, head, mn))
+  
+  tibble(text = grams)
+}
+f3()
+
+
+# tidytext implementation
+f4 <- function(data_frame = df){
+  data_frame %>% 
+    unnest_tokens(input = value, output = text, token = "ngrams", n = 2, drop = TRUE)
+}
+f4()
+
+
+
+
+
+
+# PERFORMANCE EVALUTATION ------------------------------------------------
+microbenchmark(
+  f1(),
+  
+  f2(),
+  
+  f3(),
+  
+  f4(),
+  
+  times = 1000, check = "identical"
+)
+
+# BEFORE REQUIRING ALL FUNCTIONS TO RETURN IDENTICAL OUTPUT
+# (f's 1, 2, and 3 returned character vectors)
+#
+# (microseconds)
+# expr   min      lq      mean  median     uq     max neval
+# f1()  33.4   45.20   58.3125   53.50   63.3   302.8  1000
+# f2() 152.7  172.40  207.4807  191.50  209.1  1085.5  1000
+# f3()  75.7   95.15  115.6706  108.25  121.1   926.4  1000
+# f4() 970.4 1016.30 1211.6472 1056.65 1214.0 20109.6  1000
+
+
+# AFTER REQUIRING ALL FUNCTIONS TO REUTRN IDENTICAL OUTPUT
+#
+# (microseconds)
+# expr   min      lq      mean  median      uq     max neval
+# f1() 456.4  477.85  603.8260  506.70  582.45 15474.9  1000
+# f2() 601.3  636.35  855.5795  675.45  772.50 16002.6  1000
+# f3() 496.4  525.05  683.0014  555.25  655.85  5008.8  1000
+# f4() 969.8 1086.25 1337.5965 1182.95 1310.70  5609.6  1000
